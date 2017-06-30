93c8f37eb80c74e265059d68c8ddc9b84eba3fb8
mekatama <tamatte@hotmail.com>
2017-06-30 11:37:42 +0900

    画面タップを取得

---
+++ Tap.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tap : MonoBehaviour {
+	public bool gamenTap;
+
+	// Update is called once per frame
+	void Update () {
+		//タップした判定
+		if(Input.GetMouseButtonDown(0)){
+			gamenTap = true;
+			Debug.Log("tap:" + gamenTap);
+		}
+		//離した判定
+		if(Input.GetMouseButtonUp(0)){
+			gamenTap = false;
+			Debug.Log("tap:" + gamenTap);
+		}
+	}
+}
