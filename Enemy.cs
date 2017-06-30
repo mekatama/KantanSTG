2d4e8ca2c51e9d958bcb7b51ee46c9a8a7643ea9
mekatama <tamatte@hotmail.com>
2017-06-30 12:22:45 +0900

    敵の画面下での消去処理を入れる

---
+++ Enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "MissLine"){
+			//このGameObjectを［Hierrchy］ビューから削除する
+			Destroy(gameObject);
+		}
+	}
+}
