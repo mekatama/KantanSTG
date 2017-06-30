6a1e562a5de887b449b8c88217df65f20dcd2aa3
mekatama <tamatte@hotmail.com>
2017-06-30 12:51:38 +0900

    連射数制御を入れる

--- Bullet.cs
+++ Bullet.cs
@@ -3,21 +3,19 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 	public float bulletMoveSpeed = 10.0f;	//1秒間に弾が進む距離
+	GameObject playey;						//検索したオブジェクト入れる用
 
 	void Start(){
 		GetComponent<Rigidbody2D>().velocity = new Vector2(0, bulletMoveSpeed);
-	}
-
-	void Update() {
-		//1秒間の移動量
-//		Vector2 vecAddPos = (transform.up * bulletMoveSpeed);
-		//移動量、回転量には Time.deltaTime をかけて実行環境(フレーム数の差)による違いが出ないようにします
-//		transform.position += (vecAddPos) * Time.deltaTime);
+		playey = GameObject.FindWithTag ("Player");		//Playerタグのオブジェクトを探す
 	}
 
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.tag == "Wall"){
+			//pって仮の変数にPlayerコンポーネントを入れる
+			Player p = playey.GetComponent<Player>();
+			p.rapidNum -= 1;
 			//このGameObjectを［Hierrchy］ビューから削除する
 			Destroy( gameObject);
 		}
