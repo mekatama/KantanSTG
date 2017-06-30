cbf95bc9b2c4848cd4cd9730008cab4345d33730
mekatama <tamatte@hotmail.com>
2017-06-30 16:34:14 +0900

    スコア表示用のスクリプト

---
+++ UI_Score.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_Score : MonoBehaviour {
+	public Text scoreLabel;	//スコア表示をするオブジェクト用
+	GameObject gameController;	//検索したオブジェクト入れる用
+
+	void Start () {
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		scoreLabel.text = gc.totalScore.ToString("000000");	
+	}
+}
