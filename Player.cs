6fb3e4fea4ab1d882a8b78fee44403a1bb61d1f9
mekatama <tamatte@hotmail.com>
2017-06-30 11:37:06 +0900

    自機の左右移動を入れる

--- Player.cs
+++ Player.cs
@@ -2,17 +2,49 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-	GameObject gameController;	//検索したオブジェクト入れる用
-	public int moveSpeed = 2;	//移動速度
+	GameObject playey;								//検索したオブジェクト入れる用
+	public int moveSpeed = 2;						//移動速度
+	public GameObject bulletObject = null;			//弾プレハブ
+	GameObject tapArea;								//検索したオブジェクト入れる用
+	private bool shotFlag = false;					//ショット一回だけ発射処理用フラグ
+	public Transform bulletStartPosition = null;	//弾の発射位置を取得するボーン
 
 	void Start () {
-		gameController = GameObject.FindWithTag ("Player");	//Playerタクのオブジェクトを探す
+		playey = GameObject.FindWithTag ("Player");		//Playerタグのオブジェクトを探す
+		tapArea = GameObject.FindWithTag ("TapArea");	//TapAreaタグのオブジェクトを探す
 	}
 	
 	void FixedUpdate () {
 		//rbって仮の変数にRigidbody2Dコンポーネントを入れる
-		Rigidbody2D rb = gameController.GetComponent<Rigidbody2D>();
-		//移動させる
-		rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+		Rigidbody2D rb = playey.GetComponent<Rigidbody2D>();
+		//移動させる(左右反転処理している)
+		rb.velocity = new Vector2(transform.localScale.x * moveSpeed, rb.velocity.y);
+
+		//rbって仮の変数にRigidbody2Dコンポーネントを入れる
+		Tap t = tapArea.GetComponent<Tap>();
+		//自機の弾発射ふらぐと、Tapのフラグを見て弾を発射する
+		if(shotFlag == false){
+			if(t.gamenTap){
+				Debug.Log("shot");
+				//弾を生成する位置を指定する
+				Vector2 vecBulletPos = bulletStartPosition.position;
+				Instantiate(bulletObject, vecBulletPos, transform.rotation);			//プレハフ生成
+				shotFlag = true;
+			}
+		}
+		//タップ離すと初期化する
+		if(t.gamenTap == false){
+			shotFlag = false;
+		}
+	}
+
+	//接触判定
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.tag == "Wall"){
+			//tempって仮の変数にlocalScaleコンポーネントを入れる
+			Vector2 temp = gameObject.transform.localScale;
+			temp.x *= -1;							//左右反転させる
+			gameObject.transform.localScale = temp;	//元に戻す
+		}
 	}
 }
