eba40665ab44f83e46a80503986efb196d2c0652
mekatama <tamatte@hotmail.com>
2017-07-07 16:47:20 +0900

    パワーアップアイテムの基本スクリプト

---
+++ Item_powerup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item_powerup : MonoBehaviour {
+	public int itemPowerUp = 1;	//パワーアップ数値
+
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player"){
+			//このGameObjectを［Hierrchy］ビューから削除する
+			Destroy(gameObject);
+		}
+	}
+
+}
