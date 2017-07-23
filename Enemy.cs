677c37a0a7c120559e864bd7d138798917c1d822
mekatama <tamatte@hotmail.com>
2017-07-23 22:13:47 +0900

    ミスラインに接触時の挙動変更

--- Enemy.cs
+++ Enemy.cs
@@ -12,10 +12,10 @@ public class Enemy : MonoBehaviour {
 
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter2D(Collider2D other) {
-		if(other.tag == "MissLine"){
-			//このGameObjectを［Hierrchy］ビューから削除する
-			Destroy(gameObject);
-		}
+//		if(other.tag == "MissLine"){
+//			//このGameObjectを［Hierrchy］ビューから削除する
+//			Destroy(gameObject);
+//		}
 		if(other.tag == "Bullet"){
 			Debug.Log("hit!!");
 			if(enemyHp > 0){
