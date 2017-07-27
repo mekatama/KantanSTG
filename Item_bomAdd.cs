0f2658ac82a9641bdd0911eaf112af664ddabf3f
mekatama <tamatte@hotmail.com>
2017-07-27 16:59:29 +0900

    取得ボム数管理用

---
+++ Item_bomAdd.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item_bomAdd : MonoBehaviour {
+	public int itemBomAdd = 1;			//ボム数
+	public GameObject gameController;	//ボム数表示を管理しているオブジェクト
+
+	void Start(){
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Player"){
+			//gcって仮の変数にGameControllerのコンポーネントを入れる
+			GameController gc = gameController.GetComponent<GameController>();
+			//ボム数加算
+			gc.totalBom = gc.totalBom + itemBomAdd;
+			//このGameObjectを［Hierrchy］ビューから削除する
+			Destroy(gameObject);
+		}
+	}
+}
