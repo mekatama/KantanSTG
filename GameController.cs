f33f3c457988abba32092826da53676859226dd4
mekatama <tamatte@hotmail.com>
2017-07-31 16:57:06 +0900

    playerタップ処理を削除

--- GameController.cs
+++ GameController.cs
@@ -5,38 +5,4 @@ public class GameController : MonoBehaviour {
 	public int totalScore = 0;	//ここで合計スコアを管理していく
 	public int totalBom = 0;	//ここでボス数を管理していく
 	public bool bomTap = false;	//playerて使用するplayer自身をタップしたかどうかのフラグ
-
-	void Update () {
-		GameObject obj = getClickObject();	//
-
-		//タップした場所の座標を取得→プレハブ生成
-		if(Input.GetMouseButtonDown(0)){
-//			Vector2 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);	//スクリーン座標
-
-			if(obj.tag == "Player"){
-//				Debug.Log("BOM!!" + obj.tag);
-				bomTap = true;
-			}else{
-//				Debug.Log("click=ETC");				
-			}
-		}
-				//離した判定
-		if(Input.GetMouseButtonUp(0)){
-			bomTap = false;
-//			Debug.Log("tap:" + gamenTap);
-		}
-	}
-
-	//タップしたオブジェクト取得関数(とりあえす、詳しい内容は知らない)
-	private GameObject getClickObject(){
-		GameObject result = null;
-		if(Input.GetMouseButtonDown(0)){
-			Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);	//スクリーン座標
-			Collider2D collition2d = Physics2D.OverlapPoint(tapPoint);
-			if(collition2d){
-				result = collition2d.transform.gameObject;
-			}
-		}
-		return result;
-	}
 }
