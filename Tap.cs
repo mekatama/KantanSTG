8edf1e5877d0878f00c8d36009a013f18d3d569a
mekatama <tamatte@hotmail.com>
2017-07-31 16:57:32 +0900

    playerタップ処理を追加

--- Tap.cs
+++ Tap.cs
@@ -2,19 +2,39 @@
 using System.Collections;
 
 public class Tap : MonoBehaviour {
-	public bool gamenTap;
+	public bool gamenTap = false;	//タップエリアをタップしたかどうかのフラグ
+	public bool bomTap = false;		//playerをタップしたかどうかのフラグ
 
-	// Update is called once per frame
 	void Update () {
+		GameObject obj = getClickObject();	//タップしたオブジェクトを入れる
 		//タップした判定
 		if(Input.GetMouseButtonDown(0)){
-			gamenTap = true;
-//			Debug.Log("tap:" + gamenTap);
+			if(obj.tag == "Player"){
+//				Debug.Log("BOM!!");
+				bomTap = true;
+			}else{
+//				Debug.Log("gamenTap");				
+				gamenTap = true;
+			}
 		}
+
 		//離した判定
 		if(Input.GetMouseButtonUp(0)){
+			bomTap = false;
 			gamenTap = false;
-//			Debug.Log("tap:" + gamenTap);
 		}
+	}
+
+	//タップしたオブジェクト取得関数(とりあえす、詳しい内容は知らない)
+	private GameObject getClickObject(){
+		GameObject result = null;
+		if(Input.GetMouseButtonDown(0)){
+			Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);	//スクリーン座標
+			Collider2D collition2d = Physics2D.OverlapPoint(tapPoint);
+			if(collition2d){
+				result = collition2d.transform.gameObject;
+			}
+		}
+		return result;
 	}
 }
