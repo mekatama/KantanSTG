dbefd4fc8dd5ab3ba50d32e2a4fef0f2a8fc8f48
mekatama <tamatte@hotmail.com>
2017-08-02 15:31:21 +0900

    フェンスの点滅処理を入れる

--- MissLineFall.cs
+++ MissLineFall.cs
@@ -5,7 +5,35 @@ using UnityEngine;
 public class MissLineFall : MonoBehaviour {
 	private int enemyTamaruNum;	//敵がたまった数
 
+	private float nextTime;
+	public float interval = 1.0f;	//点滅周期
+	public Renderer rend;			//
+	GameObject gameController;						//検索したオブジェクト入れる用
+
+	void Start () {
+		nextTime = Time.time;				//現時刻を代入
+		rend = GetComponent<Renderer>();	//点滅させたいオブジェクトを入れる用
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+
 	void Update(){
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+
+		//フェンスに接触した敵の数によって、フェンス点滅
+		if(!gc.gameOver){
+		if(enemyTamaruNum == 2){
+			//呼び出されたら
+			if(Time.time > nextTime){
+				rend.enabled = !rend.enabled;	//表示反転
+				nextTime += interval;			//次に点滅する時間をセット
+			}else{
+				rend.enabled = true;
+			}
+			Debug.Log("blink");
+		}
+		}
+
 		//フェンスに接触した敵の数によって、フェンス落下
 		if(enemyTamaruNum == 3){
 			//FreezePositionを無効化する
