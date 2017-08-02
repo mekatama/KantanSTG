d0381cc72fad3e912bc1bd32913881fa6faa827d
mekatama <tamatte@hotmail.com>
2017-08-02 15:30:59 +0900

    作ったけど未使用にした

---
+++ MissLineBlink.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissLineBlink : MonoBehaviour {
+	private float nextTime;
+	public float interval = 1.0f;	//点滅周期
+	public Renderer rend;			//
+
+	void Start () {
+		nextTime = Time.time;				//現時刻を代入
+		rend = GetComponent<Renderer>();	//点滅させたいオブジェクトを入れる用
+	}
+	
+	void Update () {
+		//呼び出されたら
+		if(Time.time > nextTime){
+			rend.enabled = !rend.enabled;	//表示反転
+			nextTime += interval;			//次に点滅する時間をセット
+		}
+	}
+}
