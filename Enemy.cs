4824a7e2f5f4671a7eb4e3f94e24b233dbb68b0c
mekatama <tamatte@hotmail.com>
2017-08-04 14:22:58 +0900

    ちょっと迷走中。一時保存

--- Enemy.cs
+++ Enemy.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
+	GameObject enemy;				//検索したオブジェクト入れる用
 	public int enemyHp;				//敵のHP
 	public int enemyScore;			//敵のスコア
+	public int moveSpeed = 2;		//enemyの左右移動速度
 	GameObject gameController;		//検索したオブジェクト入れる用
 	public GameObject item = null;	//Enemyから出現させるアイテム
 
 	void Start(){
+		enemy = GameObject.FindWithTag ("Enemy");					//Enemyタグのオブジェクトを探す
 		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
 	}
+
+	void Update () {
+		//rbって仮の変数にRigidbody2Dコンポーネントを入れる
+//		Rigidbody2D rb = enemy.GetComponent<Rigidbody2D>();
+		//移動させる(左右反転処理している)
+//		rb.velocity = new Vector2(transform.localScale.x * moveSpeed, rb.velocity.y);
+	}
+
 
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter2D(Collider2D other) {
