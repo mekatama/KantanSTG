7396bf50e4c0ac76388354797c4f42782ca325f2
mekatama <tamatte@hotmail.com>
2017-08-04 18:51:42 +0900

    enemyの効果音設定

--- Enemy.cs
+++ Enemy.cs
@@ -2,36 +2,34 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-//	GameObject enemy;				//検索したオブジェクト入れる用
 	public int enemyHp;				//敵のHP
 	public int enemyScore;			//敵のスコア
-//	public int moveSpeed = 2;		//enemyの左右移動速度
 	GameObject gameController;		//検索したオブジェクト入れる用
 	public GameObject item = null;	//Enemyから出現させるアイテム
+	AudioSource audioSource;			//AudioSourceコンポーネント取得用
+	public AudioClip audioClipHit;		//HitSE
+	public AudioClip audioClipDestroy;	//DestroySE
 
 	void Start(){
-//		enemy = GameObject.FindWithTag ("Enemy");					//Enemyタグのオブジェクトを探す
 		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+		audioSource = gameObject.GetComponent<AudioSource>();		//AudioSourceコンポーネント取得
 	}
-
-	void Update () {
-		//rbって仮の変数にRigidbody2Dコンポーネントを入れる
-//		Rigidbody2D rb = enemy.GetComponent<Rigidbody2D>();
-		//移動させる(左右反転処理している)
-//		rb.velocity = new Vector2(transform.localScale.x * moveSpeed, rb.velocity.y);
-	}
-
 
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter2D(Collider2D other) {
 		//通常弾との判定
 		if(other.tag == "Bullet"){
+			audioSource.clip = audioClipHit;				//SE決定
+			audioSource.Play ();							//SE再生
 //			Debug.Log("hit!!");
 			if(enemyHp > 0){
 				enemyHp -= 1;
 //				Debug.Log("EneyHP=" + enemyHp);
 			}
 			if(enemyHp <= 0){
+//				audioSource.clip = audioClipDestroy;			//SE決定
+				AudioSource.PlayClipAtPoint( audioClipDestroy, transform.position);	//SE再生(Destroy対策用)
+//				audioSource.Play ();							//SE再生
 				//gcって仮の変数にGameControllerのコンポーネントを入れる
 				GameController gc = gameController.GetComponent<GameController>();
 				gc.totalScore = gc.totalScore + enemyScore;
