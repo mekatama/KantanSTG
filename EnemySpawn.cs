06ca0928f6bf2cc2d028ebd869a5ea120e085de0
mekatama <tamatte@hotmail.com>
2017-08-04 15:39:26 +0900

    敵の配列対応

--- EnemySpawn.cs
+++ EnemySpawn.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 
 public class EnemySpawn : MonoBehaviour {
-	public GameObject enemyObject = null;	//敵のプレハブ
+//	public GameObject enemyObject = null;	//敵のプレハブ
+	public GameObject[] enemyObject;	//敵のプレハブを配列で管理
+	public GameObject enemyHolder;		//敵のプレハブをヒエラルキー画面でまとめるため
+
 	public float timeOut;					//敵を出現させたい時間間隔
 	private float timeElapsed;				//時間を仮に格納する変数
 	
@@ -18,7 +21,15 @@ public class EnemySpawn : MonoBehaviour {
 		//ランダムで出現位置を決める
 		float x_pos = Random.Range(-2.0f,2.0f); 
 		//敵を生成する
-		Instantiate(enemyObject,new Vector2(x_pos, transform.position.y),transform.rotation);	//プレハフ生成
+		GameObject enemy = (GameObject)Instantiate(	//プレハフ生成
+			enemyObject[0],	//■仮で0を入れている。0～4を想定
+			new Vector2(x_pos, transform.position.y),
+			transform.rotation
+		);
+
+		//親オブジェクトの設定
+		enemy.transform.parent = enemyHolder.transform;
+	
 		timeElapsed = 0.0f;			//リセット
 	}
 }
