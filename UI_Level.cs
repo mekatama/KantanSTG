1289a6ec75b807807b4041670a9fa50c6647839d
mekatama <tamatte@hotmail.com>
2017-08-08 17:05:03 +0900

    難易度管理用の変数追加

---
+++ UI_Level.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Level : MonoBehaviour {
+	public Text LevelLabel;		//レベル表示をするオブジェクト用
+	GameObject gameController;	//検索したオブジェクト入れる用
+
+	// Use this for initialization
+	void Start () {
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		LevelLabel.text = gc.totalLevel.ToString("000000");	
+	}
+}
