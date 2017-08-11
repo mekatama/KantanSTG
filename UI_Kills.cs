e6655218a82fd4ae23f50ada2f01c847d23754b2
mekatama <tamatte@hotmail.com>
2017-08-11 12:41:27 +0900

    敵の撃破数追加

---
+++ UI_Kills.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Kills : MonoBehaviour {
+	public Text KillsLabel;	//レベル表示をするオブジェクト用
+	GameObject gameController;	//検索したオブジェクト入れる用	
+
+	void Start () {
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		KillsLabel.text = gc.totalKills.ToString("000000");	
+	}
+}
