fea6f3df20034536678b9c66ca2112545a63fec0
mekatama <tamatte@hotmail.com>
2017-08-11 12:42:40 +0900

    撃破数とボムの使用数変数追加

--- GameController.cs
+++ GameController.cs
@@ -3,21 +3,23 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
-	public int totalScore = 0;	//ここで合計スコアを管理していく
-	public int totalBom = 0;	//ここでボス数を管理していく
-	public int totalLevel = 0;	//ここで難易度を管理していく
-	public bool bomTap = false;	//playerて使用するplayer自身をタップしたかどうかのフラグ
+	public int totalScore = 0;		//ここで合計スコアを管理していく
+	public int totalBom = 0;		//ここでボス数を管理していく
+	public int totalLevel = 0;		//ここで難易度を管理していく
+	public int totalKills = 0;		//ここで撃破数を管理していく
+	public int totalBomAttack = 0;	//ここでボム使用数を管理していく
+	public bool bomTap = false;		//playerて使用するplayer自身をタップしたかどうかのフラグ
 
 	public Canvas gameoverCamvas;
 	public bool gameOver = false;	//GameOver用のフラグ。player側て制御する
 
 	void Start () {
-			gameoverCamvas.enabled = false;
+		gameoverCamvas.enabled = false;	//GameOverUI非表示
 	}	
 	void Update () {
 		if(gameOver){
 			Debug.Log("gameover");
-			gameoverCamvas.enabled = true;
+			gameoverCamvas.enabled = true;	//GameOverUI表示
 		}
 	}
 }
