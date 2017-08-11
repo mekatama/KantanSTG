adcccc51ab5d29eee254cf106f3ae432bbda29b8
mekatama <tamatte@hotmail.com>
2017-08-11 15:29:01 +0900

    難易度で出現する敵の種類を決める

--- EnemySpawn.cs
+++ EnemySpawn.cs
@@ -7,13 +7,15 @@ public class EnemySpawn : MonoBehaviour {
 	public float timeOut;				//敵を出現させたい時間間隔
 	private float timeElapsed;			//時間を仮に格納する変数
 	public int moveSpeed = 1;			//enemyの左右移動速度
-	GameObject gameController;			//検索したオブジェクト入れる用	
+	GameObject gameController;			//検索したオブジェクト入れる用
+	private int eType;					//enemyの種類
 
 	void Start () {
 		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
 	}
 
 	void Update () {
+		//時間チェック
 		timeElapsed += Time.deltaTime;	//経過時間の保存
         if(timeElapsed >= timeOut) {	//指定した経過時間に達したら
 			EnemyGo();
@@ -23,17 +25,29 @@ public class EnemySpawn : MonoBehaviour {
 	public void EnemyGo(){
 		//ランダムで出現位置を決める
 		float x_pos = Random.Range(-2.0f,2.0f); 
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+
+		//難易度で出現する敵の種類を決める
+		if(gc.totalLevel < 3){
+			eType = 0;
+		}else if(gc.totalLevel >= 3 && gc.totalLevel < 6){
+			eType = Random.Range(0,2);
+		}else if(gc.totalLevel >= 6 && gc.totalLevel < 9){
+			eType = Random.Range(0,3);
+		}else if(gc.totalLevel >= 9){
+			eType = Random.Range(0,4);
+		}
+
 		//敵を生成する
-		GameObject enemy = (GameObject)Instantiate(	//プレハフ生成
-			enemyObject[0],	//■仮で0を入れている。0～4を想定
+		GameObject enemy = (GameObject)Instantiate(
+			enemyObject[eType],	//■仮で0を入れている。0～4を想定
 			new Vector2(x_pos, transform.position.y),
 			transform.rotation
 		);
 		//親オブジェクトの設定
 		enemy.transform.parent = enemyHolder.transform;
 
-		//gcって仮の変数にGameControllerのコンポーネントを入れる
-		GameController gc = gameController.GetComponent<GameController>();
 		//生成時にaddforce
 		//EnemyのRigidbodyを取得して、左右移動する
 		Rigidbody2D enemyRigidbody = enemy.GetComponent<Rigidbody2D>();	//コンポーネント取得
