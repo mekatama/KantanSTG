09acd53115a985a1fd9b73b3a7434b21180d0fc9
mekatama <tamatte@hotmail.com>
2017-08-11 15:03:59 +0900

    難易度で落下速度変化
    ついでに左右移動のランダム化

--- EnemySpawn.cs
+++ EnemySpawn.cs
@@ -7,8 +7,12 @@ public class EnemySpawn : MonoBehaviour {
 	public float timeOut;				//敵を出現させたい時間間隔
 	private float timeElapsed;			//時間を仮に格納する変数
 	public int moveSpeed = 1;			//enemyの左右移動速度
-	
-	// Update is called once per frame
+	GameObject gameController;			//検索したオブジェクト入れる用	
+
+	void Start () {
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+
 	void Update () {
 		timeElapsed += Time.deltaTime;	//経過時間の保存
         if(timeElapsed >= timeOut) {	//指定した経過時間に達したら
@@ -28,12 +32,20 @@ public class EnemySpawn : MonoBehaviour {
 		//親オブジェクトの設定
 		enemy.transform.parent = enemyHolder.transform;
 
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
 		//生成時にaddforce
 		//EnemyのRigidbodyを取得して、左右移動する
 		Rigidbody2D enemyRigidbody = enemy.GetComponent<Rigidbody2D>();	//コンポーネント取得
-		//左右移動、仮の数値
-		enemyRigidbody.AddForce(new Vector2(10, 0));
-
+		//左右移動、難易度でアップ(左右はランダム)
+		//難易度で落下速度アップ
+		int LRran = Random.Range(0,2);
+		if(LRran == 0){
+			enemyRigidbody.AddForce(new Vector2(gc.totalLevel, gc.totalLevel));
+		}else{
+			enemyRigidbody.AddForce(new Vector2(gc.totalLevel * -1, gc.totalLevel));
+		}
+	
 		timeElapsed = 0.0f;			//リセット
 	}
 }
