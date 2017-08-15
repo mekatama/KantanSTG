087dfd31bcc08dd935287a1d44e75ff595870013
mekatama <tamatte@hotmail.com>
2017-08-15 16:50:47 +0900

    不要なコード削除

--- BulletBom.cs
+++ BulletBom.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class BulletBom : MonoBehaviour {
 	public float bulletMoveSpeed = 10.0f;	//1秒間に弾が進む距離
-//	public bool bomDestroy = false;			//BOMか消えたフラグ
 	GameObject player;						//検索したオブジェクト入れる用
 
 	void Start(){
@@ -14,17 +13,11 @@ public class BulletBom : MonoBehaviour {
 
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter2D(Collider2D other) {
-
 		if(other.tag == "Wall"){
 			//pって仮の変数にPlayerコンポーネントを入れる
 			Player p = player.GetComponent<Player>();
 			p.bomFlag = false;		//PlayerのBOM発射フラグをoff
-//			bomDestroy = true;		//BOMか消えたフラグon。playerで使用する
 			Destroy( gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
-		}
-
-		if(other.tag == "Enemy"){
-//			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
 		}
 	}
 }
