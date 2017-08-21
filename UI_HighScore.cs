54538a90556868cb0f4087097c6e13a8888017f6
mekatama <tamatte@hotmail.com>
2017-08-21 10:55:25 +0900

    タイトル画面にハイスコア表示

---
+++ UI_HighScore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_HighScore : MonoBehaviour {
+	public Text highScoreLabel;	//ハイスコア用のtext
+
+	void Start () {
+		//ハイスコアの表示
+		highScoreLabel.text = "HighScore:" + PlayerPrefs.GetInt("HighScore");
+	}
+	
+	void Update () {
+		
+	}
+}
