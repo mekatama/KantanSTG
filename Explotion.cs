41633ca244fb9a9ee76b66416ab766a5e3382942
mekatama <tamatte@hotmail.com>
2017-08-23 17:20:52 +0900

    爆発消去のスクリプト

---
+++ Explotion.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explotion : MonoBehaviour {
+	public void DeleteExplosion (){
+		Destroy(gameObject);	//オブジェクトの削除
+	}
+}
