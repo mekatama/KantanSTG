a74d34b5668cc08a5d80b6978d93932b12416532
mekatama <tamatte@hotmail.com>
2017-08-30 18:30:55 +0900

    ワーニングSEを鳴らす制御

--- MissLineFall.cs
+++ MissLineFall.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MissLineFall : MonoBehaviour {
-	private int enemyTamaruNum;		//敵がたまった数
+	private int enemyTamaruNum;			//敵がたまった数
 	private float nextTime;
-	public float interval = 1.0f;	//点滅周期
-	public Renderer rend;			//点滅させたいオブジェクトを入れる用
-	GameObject gameController;		//検索したオブジェクト入れる用
-	AudioSource audioSource;		//AudioSourceコンポーネント取得用
-
-	private bool warningFlag = false;
+	public float interval = 1.0f;		//点滅周期
+	public Renderer rend;				//点滅させたいオブジェクトを入れる用
+	GameObject gameController;			//検索したオブジェクト入れる用
+	AudioSource audioSource;			//AudioSourceコンポーネント取得用
+	private bool warningFlag = false;	//SE再生制御用
 
 	void Start () {
 		nextTime = Time.time;										//現時刻を代入
 		rend = GetComponent<Renderer>();							//点滅させたいオブジェクトを入れる用
 		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
-		audioSource = GetComponent<AudioSource>();	//AudioSourceコンポーネント取得
+		audioSource = GetComponent<AudioSource>();					//AudioSourceコンポーネント取得
 	}
 
 	void Update(){
@@ -30,20 +29,21 @@ public class MissLineFall : MonoBehaviour {
 					rend.enabled = !rend.enabled;	//表示反転
 					nextTime += interval;			//次に点滅する時間をセット
 					if(warningFlag == false){
-						audioSource.Play ();				//SE再生
+						audioSource.Play ();		//SE再生
 						warningFlag = true;			
 					}
 				}else{
 					rend.enabled = true;			//強制的に表示on
 					warningFlag = false;
 				}
-				Debug.Log("blink");
+			}else{
+				audioSource.Stop ();				//SE停止
 			}
 		}
 
 		//フェンスに接触した敵の数によって、フェンス落下
 		if(enemyTamaruNum == 3){
-			audioSource.Stop ();			//SE停止
+			audioSource.Stop ();		//SE停止
 			rend.enabled = true;		//表示on
 			//FreezePositionを無効化する
 			gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
@@ -54,7 +54,6 @@ public class MissLineFall : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.tag == "Enemy"){
 			enemyTamaruNum += 1;
-//			Debug.Log("tamaru = " + enemyTamaruNum);
 		}
 	}
 
@@ -62,8 +61,7 @@ public class MissLineFall : MonoBehaviour {
 	void OnTriggerExit2D(Collider2D other) {
 		if(other.tag == "Enemy"){
 			enemyTamaruNum -= 1;
-			rend.enabled = true;		//表示on
-//			Debug.Log("tamaru = " + enemyTamaruNum);
+			rend.enabled = true;	//表示on
 		}
 	}
 }
